af26f1a8a898e2798b17f3e72658fc7317ee4283
Shashi Kumar 3 <shashikumar.sk0@gmail.com>
2023-07-15 20:40:58 +0100

    test

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Test
           </Typography>
           <Button
             color="primary"
